2a022a7e7400f765d3447e68177df4b2fe41b444
Jeffgmw <108535585+Jeffgmw@users.noreply.github.com>
2024-02-21 16:10:52 +0300

    first

---
+++ src/main/java/com/teller/BackEndTellerAutomation/BackEndTellerAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.teller.BackEndTellerAutomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndTellerAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndTellerAutomationApplication.class, args);
+	}
+
+}
